e6f4d2bf5b9f7f66e7a6240405f375c6d2543937
Kevin Shirley <kevinxshirley@gmail.com>
2024-01-23 12:49:20 -0500

    selectedColorCode used in drawer body

--- src/components/products/product-drawer/body.tsx
+++ src/components/products/product-drawer/body.tsx
@@ -1,6 +1,7 @@
 'use clint'
 
 import Image from 'next/image'
+import { isNil } from 'lodash'
 import { ProductType } from '@/interfaces/product'
 import DrawerTable from '@/components/products/product-drawer/table'
 import { useAppContext } from '@/context/app-context'
@@ -18,23 +19,27 @@ export default function DrawerBody({ product }: DrawerBodyType) {
     <section className='pt-4 rounded-b-xl flex justify-center'>
       <div className='flex flex-col lg:flex-row justify-between items-center gap-x-4'>
         <div className='max-w-xs mb-6' style={{ maxHeight: '35rem' }}>
-          <div className='rounded-lg shadow-xl rounded-b-lg'>
-            <Image
-              className='rounded-t-xl min-w-xs'
-              src={product.styleImageUrl}
-              alt='Product image'
-              width={300}
-              height={48}
-              priority
-            />
-            <div className='flex flex-col py-4 px-4'>
-              <span className='bold font-bold text-lg'>{product.colorCodes[0].description}</span>
-              <span className='text-lg'>{product.colorCodes[0].currentPrice}</span>
+          {!isNil(selectedColorCode) && (
+            <div className='rounded-lg shadow-xl rounded-b-lg'>
+              <Image
+                className='rounded-t-xl min-w-xs'
+                src={product.styleImageUrl}
+                alt='Product image'
+                width={300}
+                height={48}
+                priority
+              />
+              <div className='flex flex-col py-4 px-4'>
+                <span className='bold font-bold text-lg'>{selectedColorCode.description}</span>
+                <span className='text-lg'>{selectedColorCode.currentPrice}</span>
+              </div>
             </div>
-          </div>
+          )}
         </div>
         <div className='max-w-xs md:max-w-lg lg:max-w-full overflow-x-auto'>
-          <DrawerTable product={product} />
+          {!isNil(product) && (
+            <DrawerTable product={product} />
+          )}
         </div>
       </div>
     </section>
